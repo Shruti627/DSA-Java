fa705f688a55362d6722ac1c483528760fb8ddad
Shruti627 <shrutisangvikar627@gmail.com>
2026-03-27 13:01:14 +0530

    move zeros

---
+++ ARRAY/Array_1/MoveZeros.java
@@ -0,0 +1,28 @@
+public class MoveZeros {
+    public static void moveZerosToEnd(int[] arr) {
+        int index = 0; 
+
+       
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+
+        
+        while (index < arr.length) {
+            arr[index] = 0;
+            index++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 3, 12};
+        moveZerosToEnd(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
