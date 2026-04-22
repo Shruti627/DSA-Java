3b98834da45410f697a0c003a019d2535b3af55a
Shruti627 <shrutisangvikar627@gmail.com>
2026-04-22 12:35:33 +0530

    prime

---
+++ Must do/PrimeNumber.java
@@ -0,0 +1,23 @@
+public class PrimeNumber{
+       public static void main(String[] args) {
+           int n=5;
+           boolean isPrime=true;
+
+           for(int i=2;i<=Math.sqrt(n);i++){
+
+              if(n%i==0){
+                     isPrime=false;
+                     break;
+
+              }
+           }
+
+           if(isPrime){
+              System.out.println("Prime");
+           }else{
+              System.out.println("Not Prime");
+           }
+
+         
+       }
+}
