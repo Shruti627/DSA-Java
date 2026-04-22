01dcd06694a34fbd6bdff50bc8ef8e11a5b97420
Shruti627 <shrutisangvikar627@gmail.com>
2026-04-22 13:26:41 +0530

    small fixing

--- Must do/AllDivisors.java
+++ Must do/AllDivisors.java
@@ -4,7 +4,7 @@ public class AllDivisors{
 
            for(int i=1;i<=n;i++){
               if(n%i==0){
-                     System.out.print(i+ " ");
+                     System.out.print(i+ "  ");
               }
            }
        }
