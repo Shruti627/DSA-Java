5ea8b21e81d4d334f53c0312e6d25a14607efed2
Shruti627 <shrutisangvikar627@gmail.com>
2026-04-23 22:25:04 +0530

    docs: improve usage section
    Co-authored-by: Ninad Ubale <ninadubale04@gmail.com>

---
+++ ARRAY/Array_1/Kadanes.java
@@ -0,0 +1,23 @@
+public class Kandanes{
+          public static int maxSubArray(int[] nums) {
+        int currentSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            currentSum += num;
+            maxSum = Math.max(maxSum, currentSum);
+
+            
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+
+        return maxSum;
+    }
+       public static void main(String[] args) {
+               int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(maxSubArray(nums)); 
+           
+       }
+}
